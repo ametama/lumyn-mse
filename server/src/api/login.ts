752e2493122d0ab9ca9a8ecbcb3fe6caa811762a
ametama <korscr1027@yahoo.com>
2024-12-09 12:50:49 +0100

    authorization

--- server/src/api/login.ts
+++ server/src/api/login.ts
@@ -7,7 +7,17 @@ export function login(api: Router, db: PgDriver) {
         "username": required("string"),
         "password": required("string")
     }), async (req, res) => {
-        const token = await db.session.login(req.body.username, req.body.password);
+        let token;
+        try {
+            token = await db.session.login(req.body.username, req.body.password);
+        } catch (e) {
+            if (e === "user_not_found") {
+                res.sendStatus(401);
+                return;
+            }
+            res.sendStatus(500);
+            return;
+        }
         res.status(200).send({
             err: null,
             data: token
--- server/src/main.ts
+++ server/src/main.ts
@@ -3,12 +3,38 @@ import YAML from "yaml";
 import { readFileSync } from "node:fs";
 import { api } from "./api";
 import { PgDriver } from "./database/PgDriver";
+import { User } from "./database/User";
 
 async function main() {
     const cfg = YAML.parse(readFileSync("/etc/lumynmse.yml").toString());
     const db = await PgDriver(cfg.postgres);
     const app = express();
     app.use(express.json());
+    app.use(async (req, res, next) => {
+        const authHeader = req.header("Authorization");
+        if (authHeader === undefined) {
+            res.sendStatus(401);
+            return;
+        }
+        const [scheme, token] = authHeader.split(" ");
+        if (scheme !== "Bearer") {
+            res.sendStatus(400);
+            return;
+        }
+        let user: User;
+        try {
+            user = await db.session.authorize(token);
+        } catch (e) {
+            if (e === "token_invalid") {
+                res.sendStatus(401);
+                return;
+            }
+            res.sendStatus(500);
+            return;
+        }
+        res.locals = { user };
+        return next();
+    });
     api(app, db);
     app.listen(cfg.port, () => console.log(`Listening on port ${cfg.port}!`));
 }
